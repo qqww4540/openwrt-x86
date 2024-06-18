c4b0ad3cd86f6b5ccec77eec884c59fcada8af0a
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-06-18 15:31:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,6 +24,7 @@ curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/powero
 
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/applications/luci-app-adguardhome && rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/mosdns
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 git clone https://github.com/kenzok8/small package/small
