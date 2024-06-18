8e6dda6eb0273f51be70d8718137602fdac6abb1
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-06-18 15:48:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,5 +26,6 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-adguardhome && rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-smartdns && rm -rf feeds/packages/net/smartdns
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 git clone https://github.com/kenzok8/small package/small
