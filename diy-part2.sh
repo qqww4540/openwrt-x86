cb82e07caec385eafd6c14e3d403b2f9ac0bbdc4
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2024-10-02 00:05:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,6 +28,7 @@ rm -rf feeds/luci/applications/luci-app-adguardhome && rm -rf feeds/packages/net
 rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-smartdns && rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 git clone https://github.com/kenzok8/small package/small
+git clone -b main --single-branch https://github.com/morytyann/OpenWrt-mihomo
